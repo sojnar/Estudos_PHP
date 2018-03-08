847dc40c8685fd3830cca48a1be1854d4212a18c
sojnar <ranjos@globomail.com>
2018-03-08 15:45:13 +0000

    ticket #10001 Iniciando o projeto matriz | BUrbano

---
+++ PHP/projeto/eduFinanceira.php
@@ -0,0 +1,62 @@
+<?php
+// Cadastro de usuários:
+// Esse programa esta abto a cadastrar 10 usuário onde cada um 
+// irá armazenar as seguintes informações:
+// Nome, e-mail, Usuario , Senha
+$col = "10";
+$vet = "0";
+
+system("clear");
+echo "Esse é um programa para controle financeiro!\n";
+echo "Digite 1 para cadastrar um usuário ou 0 para sair: ";
+$cadastra = trim(fgets(STDIN));
+$col = "10";
+
+if("$cadastra" == "1")
+{
+  do{
+      if("$vet" < "$col")
+      {
+        $vet++;
+        echo "$vet \n";
+        $j=0;
+
+        echo "Digite o seu Nome: \n";
+        $array[$vet][$j] = trim(fgets(STDIN));
+        $j++;
+        
+        echo "Digite o Email: \n";
+        $array[$vet][$j] = trim(fgets(STDIN));
+        $j++;
+
+        echo "Digite o Usuário: ";
+        $array[$vet][$j] = trim(fgets(STDIN));
+        $j++;
+        echo "\n";
+
+        echo "Digite a Senha: ";
+        $array[$vet][$j] = trim(fgets(STDIN));
+        echo "\n";
+
+        echo "Digite 1 para cadastrar um usuário ou 0 para sair: ";
+        $cadastra = trim(fgets(STDIN));
+    }else
+    {
+      $cadastra = "0";
+    }
+  }while($cadastra == "1");
+}
+
+//print_r($array);
+
+for ($i=1;$i<=$vet;$i++)
+{
+    for ($j=0;$j<=3;$j++)
+    {
+      echo "{$array[$i][$j]} \n";
+    }
+}
+
+?>
+
+
