a4dd859825e0a480379f78d70eeda01f0d117e37
sojnar <ranjos@globomail.com>
2018-03-09 04:29:11 +0000

    ticket #10003 | Controle financeiro | BUrbano

--- PHP/projeto/eduFinanceira.php
+++ PHP/projeto/eduFinanceira.php
@@ -3,18 +3,23 @@
 // Esse programa esta abto a cadastrar 10 usuário onde cada um 
 // irá armazenar as seguintes informações:
 // Nome, e-mail, Usuario , Senha
+
 $col = "10";
 $vet = "0";
 
+do{
+
 system("clear");
-echo "Esse é um programa para controle financeiro!\n";
-echo "Digite 1 para cadastrar um usuário ou 0 para sair: ";
+echo "Esse é um programa para controle financeiro! \n";
+echo "Digite 1 para cadastrar um usuário! \n";
+echo "Digite 2 para logar no sistema! \n";
+echo "Digite 0 para sair: ";
 $cadastra = trim(fgets(STDIN));
 
-if("$cadastra" == "1")
+if($cadastra == "1")
 {
   do{
-      if("$vet" < "$col")
+      if($vet < $col)
       {
         $vet++;
         echo "$vet \n";
@@ -35,19 +40,54 @@ if("$cadastra" == "1")
 
         echo "Digite a Senha: ";
         $array[$vet][$j] = trim(fgets(STDIN));
-
+        echo "Repita a Senha: ";
+        
         echo "Digite 1 para cadastrar um usuário ou 0 para sair: ";
         $cadastra = trim(fgets(STDIN));
-    }else
-    {
-      $cadastra = "0";
-    }
-  }while($cadastra == "1");
+      }else
+      {
+        $cadastra = "0";
+      }
+    }while($cadastra == "1");
 }
 
-//print_r($array);
+if ($cadastra == 2)
+{
+    echo "Digite o seu login: ";
+    $login = trim(fgets(STDIN));
+    
+    $i=1;
 
-for ($i=1;$i<=$vet;$i++)
+    do{
+        if($array[$i][2] == $login)
+        {
+          echo "Digite sua senha: ";
+          $senha = trim(fgets(STDIN));
+          
+          if($array[$i][3] == $senha)
+          {
+            $i=$vet;
+            echo "Faça";
+            system(pause);
+          }else{
+            echo "Senha incorreta!";
+          }
+        }
+        $i++;
+
+        if($i > $vet)
+        {
+          echo "Usuário inesistente!";
+        }
+    }while($i < $vet);
+}
+
+
+//}
+
+print_r($array);
+
+/*r ($i=1;$i<=$vet;$i++)
 {
   echo "################";
   echo "\nCadastro $i:\n";
@@ -59,7 +99,12 @@ for ($i=1;$i<=$vet;$i++)
   echo "################\n";
   echo "\n";
 }
+ */
 
+//  system(clear);
+  echo "Digite 1 para efetuar mais uma transação ou 0 para sair: ";
+  $sair = trim(fgets(STDIN));
+}while($sair == 1);
 ?>
 
 
--- PHP/projeto/teste.php
+++ PHP/projeto/teste.php
@@ -1,11 +1,5 @@
-<?php
+#!/bin/bash
 
-//$vet = array();
-$vet[0][0][1]= 10;
-$vet[0][0][2]= 20;
-
-echo $vet[0][0][1];
-
-print_r ($vet);
-
-?>
+echo -n "valor:"
+read -s valor
+echo $valor
