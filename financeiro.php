a97ab13ed82e8cb6ba55c321ca184851dcce4538
sojnar <ranjos@globomail.com>
2018-03-25 06:27:32 +0000

    ticket #10001 | Criação do codigo usando funcao

--- financeiro.php
+++ financeiro.php
@@ -1,16 +1,17 @@
 <?php
 
 $matriz = array();
-$vet = "0";
-$col = "10";
+$vet = "1";
 
 function menu(){
   system("clear");
   echo "Esse é um programa para controle financeiro! \n";
   echo "Digite 1 para cadastrar um usuário! \n";
   echo "Digite 2 para logar no sistema! \n";
-  echo "Digite 0 para sair: ";
+  echo "Digite 0 para sair! \n";
+  echo "Entre com a opção: ";
   $menu = trim(fgets(STDIN));
+  
   return $menu;
 }
 
@@ -18,12 +19,59 @@ function getPassword(){
   shell_exec('stty -echo');
   $password = trim(fgets(STDIN));
   shell_exec('stty echo');
+  
   return $password;
 }
 
-function cadastra($col, $vet, $matriz){
-  if($vet < $col)
+function getLogin($matriz, $vet, $j){
+  if($vet == 1)
   {
+    echo "Digite seu login: ";
+    $login = trim(fgets(STDIN));
+  }else{
+    echo "Digite seu login: ";
+    $login = trim(fgets(STDIN));
+    $result = sizeof($matriz);
+    $i=1;
+
+    do{
+      if($login == $matriz[$i][j])
+      {
+        system("clear");
+        echo "Esse login ja existe! \n";
+        echo "Digite seu login: ";
+        $login = trim(fgets(STDIN));
+        $i = 0;
+      }
+      $i++;
+    }while($i < $result);
+  }
+  
+  return $login;
+}
+
+function getVerPassword(){
+  do{
+    echo "Digite sua senha: ";
+    $passwd = getPassword();
+    echo "\nRepita sua senha: ";
+    $passwd2 = getPassword();
+
+    if($passwd != $passwd2)
+    {
+      system("clear");
+      echo "Senha incorreta!\n";
+      $sair = 0;
+    }else{
+      $var = $passwd;
+      $sair = 1;
+    }
+  }while($sair == 0);
+  
+  return $var;
+}
+
+function cadastra($col, $vet, $matriz){
     $j=0;
 
     system("clear");
@@ -35,20 +83,20 @@ function cadastra($col, $vet, $matriz){
     $matriz[$vet][$j] = trim(fgets(STDIN));
     $j++;
 
-    echo "Digite o Usuário: ";
-    $matriz[$vet][$j] = trim(fgets(STDIN));
+    $matriz[$vet][$j] = getLogin($matriz, $vet, $j);
     $j++;
 
-    echo "Digite a Senha: ";
-    $matriz[$vet][$j] = getPassword();
-//    echo "\nRepita a Senha: ";
-//    $valida = getPassword();
+    $matriz[$vet][$j] = getVerPassword();
 
     return $matriz;
-  }
+}
 
-    
-
+function getOpera(){
+  $op = array(
+    '1' => 'Deposito',
+    '2' => 'Saque'
+  );
+  return $op;
 }
 
 do{
@@ -60,6 +108,7 @@ do{
 
       do{
         $matriz = cadastra($col, $vet, $matriz);
+        echo "\n";
         print_r($matriz);
         echo "Digite 1 para continuar ou 0 para sair: ";
         $vet++;
